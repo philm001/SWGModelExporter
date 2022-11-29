68471972c2a4edec0c967025b2236262bddf632a
Phillip <phillipmobley2@gmail.com>
2022-11-29 14:22:44 -0500

    Added zip folder of DirectXTex for use on other enirvoments. Added some comments. Beginning development on static objects

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # SWGModelExporter-master
+
+Leaving a zipped copy of the DirectxTex library here for new installs.
+Extract the contents of this zipped file into the directory above the current directory.
+Also rename the folder by remoiving the wording "-main"
+
+Will be adding other items here shortly
--- mesh_file.cpp
+++ mesh_file.cpp
@@ -26,45 +26,48 @@ void LODParser::parse_data(const std::string& name, uint8_t* data_ptr, size_t da
 	}
 	else if (name == "BOX ")
 	{
-		std::vector<float> maxValues;
-		std::vector<float> minValues;
+		// Collision box. This is not needed
+		//std::vector<float> maxValues;
+		//std::vector<float> minValues;
 
-		float maxX = buffer.read_float(); // confirm these are in correct order
-		float maxY = buffer.read_float();
-		float maxZ = buffer.read_float();
+		//float maxX = buffer.read_float(); // confirm these are in correct order
+		//float maxY = buffer.read_float();
+		//float maxZ = buffer.read_float();
 
-		maxValues.push_back(maxX);
-		maxValues.push_back(maxY);
-		maxValues.push_back(maxZ);
+		//maxValues.push_back(maxX);
+		//maxValues.push_back(maxY);
+		//maxValues.push_back(maxZ);
 
-		float minX = buffer.read_float(); // confirm these are in correct order
-		float minY = buffer.read_float();
-		float minZ = buffer.read_float();
+		//float minX = buffer.read_float(); // confirm these are in correct order
+		//float minY = buffer.read_float();
+		//float minZ = buffer.read_float();
 
-		minValues.push_back(minX);
-		minValues.push_back(minY);
-		minValues.push_back(minZ);
+		//minValues.push_back(minX);
+		//minValues.push_back(minY);
+		//minValues.push_back(minZ);
 
-		m_object->AddBoundBox(maxValues);
-		m_object->AddBoundBox(minValues);
+		//m_object->AddBoundBox(maxValues);
+		//m_object->AddBoundBox(minValues);
 	}
 	else if (name == "0001SPHR")
 	{
-		std::vector<float> CenterPoint;
+		// Collision Sphere, this is also not needed in fbx
+		//std::vector<float> CenterPoint;
 
-		float cX = buffer.read_float(); // confirm these are in correct order
-		float cY = buffer.read_float();
-		float cZ = buffer.read_float();
+		//float cX = buffer.read_float(); // confirm these are in correct order
+		//float cY = buffer.read_float();
+		//float cZ = buffer.read_float();
 
-		CenterPoint.push_back(cX);
-		CenterPoint.push_back(cY);
-		CenterPoint.push_back(cZ);
+		//CenterPoint.push_back(cX);
+		//CenterPoint.push_back(cY);
+		//CenterPoint.push_back(cZ);
 
-		float radius = buffer.read_float();
+		//float radius = buffer.read_float();
 
-		m_object->AddCenterPoint(CenterPoint);
-		m_object->AddRadius(radius);
+		//m_object->AddCenterPoint(CenterPoint);
+		//m_object->AddRadius(radius);
 	}
+	/* AT this point, none of the sections after FLORDATA is needed. */
 	else if (name == "FLORDATA")
 	{
 		// do nothing for now
@@ -83,30 +86,30 @@ void LODParser::parse_data(const std::string& name, uint8_t* data_ptr, size_t da
 	}
 	else if (name == "0000VERT")
 	{
-		// Triangle vetexs
-		while (!buffer.end_of_buffer())
-		{
-			std::vector<float> triPoints;
+		/* These are not for tiagnels but for collision around the mesh*/
+		//while (!buffer.end_of_buffer())
+		//{
+		//	std::vector<float> triPoints;
 
-			float x = buffer.read_float();
-			float y = buffer.read_float();
-			float z = buffer.read_float();
+		//	float x = buffer.read_float();
+		//	float y = buffer.read_float();
+		//	float z = buffer.read_float();
 
-			triPoints.push_back(x); // confirm the order
-			triPoints.push_back(y);
-			triPoints.push_back(z);
-			                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
-			m_object->AddVertex(triPoints);
-		}
+		//	triPoints.push_back(x); // confirm the order
+		//	triPoints.push_back(y);
+		//	triPoints.push_back(z);
+		//	                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+		//	m_object->AddVertex(triPoints);
+		//}
 	}
 	else if (name == "INDX")
 	{
 		// Triangles INdicies?
-		while (!buffer.end_of_buffer())
+		/*while (!buffer.end_of_buffer())
 		{
 			uint32_t readValue = buffer.read_uint32();
 			m_object->AddIndex(readValue);
-		}
+		}*/
 	}
 }
 
@@ -129,7 +132,8 @@ void meshParser::parse_data(const std::string& name, uint8_t* data_ptr, size_t d
 	base_buffer buffer(data_ptr, data_size);
 	if (name == "0001SPHR")
 	{
-		float centerX = buffer.read_float();
+		/* MOre collision stuff */
+		/*float centerX = buffer.read_float();
 		float centerY = buffer.read_float();
 		float centerZ = buffer.read_float();
 
@@ -138,29 +142,32 @@ void meshParser::parse_data(const std::string& name, uint8_t* data_ptr, size_t d
 		std::vector<float> center{ centerX, centerY, centerZ };
 
 		m_object->setRadiusPoint(cRadius);
-		m_object->setCenter(center);
+		m_object->setCenter(center);*/
 	}
 	else if (name == "BOX ")
 	{
-		std::vector<float> maxValue;
-		std::vector<float> minValue;
+		/* More collision stuff */
+		//std::vector<float> maxValue;
+		//std::vector<float> minValue;
 
-		maxValue.push_back(buffer.read_float());// X
-		maxValue.push_back(buffer.read_float());// Y
-		maxValue.push_back(buffer.read_float());// Z
+		//maxValue.push_back(buffer.read_float());// X
+		//maxValue.push_back(buffer.read_float());// Y
+		//maxValue.push_back(buffer.read_float());// Z
 
-		minValue.push_back(buffer.read_float());// X
-		minValue.push_back(buffer.read_float());// Y
-		minValue.push_back(buffer.read_float());// Z
+		//minValue.push_back(buffer.read_float());// X
+		//minValue.push_back(buffer.read_float());// Y
+		//minValue.push_back(buffer.read_float());// Z
 
-		m_object->setMaxPoint(maxValue);
-		m_object->setMinPoint(minValue);
+		//m_object->setMaxPoint(maxValue);
+		//m_object->setMinPoint(minValue);
 	}
 	else if (name == "0001NAME")
 	{
+		/*The shader name, this is important */
 		std::string Shader = buffer.read_stringz();
 		m_object->setShaderName(Shader);
 	}
+	/* This is where the good stuff happens. Starting with INFO, we grab the flags for the verticies*/
 	else if (name == "0001INFO")
 	{
 		static bool FirstInfoHit = false;
@@ -247,6 +254,7 @@ void meshParser::parse_data(const std::string& name, uint8_t* data_ptr, size_t d
 			}
 			
 			std::vector<std::vector<float>> textureCoordinatePrime;
+
 			int testValue = m_object->GetNumTextureCoordinateSets();
 			for (int i = 0; i < m_object->GetNumTextureCoordinateSets(); i++)
 			{
@@ -280,6 +288,7 @@ void meshParser::parse_data(const std::string& name, uint8_t* data_ptr, size_t d
 	}
 	else if (name == "SIDX")
 	{
+	/* Will get to this later. The logic here will be a little different*/
 		uint32_t indexCount = buffer.read_uint32();
 		while (!buffer.end_of_buffer())
 		{
@@ -311,7 +320,7 @@ bool meshParser::is_object_parsed() const
 
 void meshObject::store(const std::string& path, const Context& context)
 {
-/*	boost::filesystem::path obj_name(m_name);
+	boost::filesystem::path obj_name(m_name);
 	boost::filesystem::path target_path(path);
 	target_path /= obj_name.filename();
 	target_path.replace_extension("fbx");
@@ -349,162 +358,5 @@ void meshObject::store(const std::string& path, const Context& context)
 	mesh_node_ptr->SetNodeAttribute(mesh_ptr);
 	scene_ptr->GetRootNode()->AddChild(mesh_node_ptr);
 
-	// prepare vertices
-	uint32_t vertices_num = static_cast<uint32_t>(m_triangleVertices.size());
-	uint32_t normals_num = static_cast<uint32_t>(m_Normals.size());
-	mesh_ptr->SetControlPointCount(vertices_num);
-	auto mesh_vertices = mesh_ptr->GetControlPoints();
 
-	for (uint32_t vertex_idx = 0; vertex_idx < vertices_num; ++vertex_idx)
-	{
-		const auto& pt = m_triangleVertices.at(vertex_idx);
-		mesh_vertices[vertex_idx] = FbxVector4(pt.at(0), pt.at(1), pt.at(2));
-	}
-
-	// add material layer
-	auto material_layer = mesh_ptr->CreateElementMaterial();
-	material_layer->SetMappingMode(FbxLayerElement::eByPolygon);
-	material_layer->SetReferenceMode(FbxLayerElement::eIndexToDirect);
-
-	// process polygons
-	std::vector<uint32_t> normal_indexes;
-	std::vector<uint32_t> tangents_idxs;
-	std::vector<Graphics::Tex_coord> uvs;
-	std::vector<uint32_t> uv_indexes;
-
-	for (uint32_t shader_idx = 0; shader_idx < m_shaders.size(); ++shader_idx)
-	{
-		auto& shader = m_shaders[shader_idx];
-		if (shader.get_definition())
-		{
-			auto material_ptr = FbxSurfacePhong::Create(scene_ptr, shader.get_name().c_str());
-			material_ptr->ShadingModel.Set("Phong");
-
-			auto& material = shader.get_definition()->material();
-			auto& textures = shader.get_definition()->textures();
-
-			// create material for this shader
-			material_ptr->Ambient.Set(FbxDouble3(material.ambient.r, material.ambient.g, material.ambient.g));
-			material_ptr->Diffuse.Set(FbxDouble3(material.diffuse.r, material.diffuse.g, material.diffuse.g));
-			material_ptr->Emissive.Set(FbxDouble3(material.emissive.r, material.emissive.g, material.emissive.g));
-			material_ptr->Specular.Set(FbxDouble3(material.specular.r, material.specular.g, material.specular.g));
-
-			// add texture definitions
-			for (auto& texture_def : textures)
-			{
-				FbxFileTexture* texture = FbxFileTexture::Create(scene_ptr, texture_def.tex_file_name.c_str());
-				boost::filesystem::path tex_path(path);
-				tex_path /= texture_def.tex_file_name;
-				tex_path.replace_extension("tga");
-
-				texture->SetFileName(tex_path.string().c_str());
-				texture->SetTextureUse(FbxTexture::eStandard);
-				texture->SetMaterialUse(FbxFileTexture::eModelMaterial);
-				texture->SetMappingType(FbxTexture::eUV);
-				texture->SetWrapMode(FbxTexture::eRepeat, FbxTexture::eRepeat);
-				texture->SetTranslation(0.0, 0.0);
-				texture->SetScale(1.0, 1.0);
-				texture->SetTranslation(0.0, 0.0);
-				switch (texture_def.texture_type)
-				{
-				case Shader::texture_type::main:
-					material_ptr->Diffuse.ConnectSrcObject(texture);
-					break;
-				case Shader::texture_type::normal:
-					material_ptr->Bump.ConnectSrcObject(texture);
-					break;
-				case Shader::texture_type::specular:
-					material_ptr->Specular.ConnectSrcObject(texture);
-					break;
-				}
-			}
-
-			mesh_ptr->GetNode()->AddMaterial(material_ptr);
-
-			// get geometry element
-			auto& triangles = shader.get_triangles();
-			auto& positions = shader.get_pos_indexes();
-			auto& normals = shader.get_normal_indexes();
-			auto& tangents = shader.get_light_indexes();
-
-			auto idx_offset = static_cast<uint32_t>(uvs.size());
-			copy(shader.get_texels().begin(), shader.get_texels().end(), back_inserter(uvs));
-
-			normal_indexes.reserve(normal_indexes.size() + normals.size());
-			tangents_idxs.reserve(tangents_idxs.size() + tangents.size());
-
-			for (uint32_t tri_idx = 0; tri_idx < triangles.size(); ++tri_idx)
-			{
-				auto& tri = triangles[tri_idx];
-				mesh_ptr->BeginPolygon(shader_idx, -1, shader_idx, false);
-				for (size_t i = 0; i < 3; ++i)
-				{
-					auto remapped_pos_idx = positions[tri.points[i]];
-					mesh_ptr->AddPolygon(remapped_pos_idx);
-
-					auto remapped_normal_idx = normals[tri.points[i]];
-					normal_indexes.emplace_back(remapped_normal_idx);
-
-					if (!tangents.empty())
-					{
-						auto remapped_tangent = tangents[tri.points[i]];
-						tangents_idxs.emplace_back(remapped_tangent);
-					}
-					uv_indexes.emplace_back(idx_offset + tri.points[i]);
-				}
-				mesh_ptr->EndPolygon();
-			}
-		}
-	}
-	// add UVs
-	FbxGeometryElementUV* uv_ptr = mesh_ptr->CreateElementUV("UVSet1");
-	uv_ptr->SetMappingMode(FbxGeometryElement::eByPolygonVertex);
-	uv_ptr->SetReferenceMode(FbxGeometryElement::eIndexToDirect);
-
-	std::for_each(uvs.begin(), uvs.end(), [&uv_ptr](const Graphics::Tex_coord& coord)
-		{
-			uv_ptr->GetDirectArray().Add(FbxVector2(coord.u, coord.v));
-		});
-	std::for_each(uv_indexes.begin(), uv_indexes.end(), [&uv_ptr](const uint32_t idx)
-		{
-			uv_ptr->GetIndexArray().Add(idx);
-		});
-
-	// add normals
-	if (!normal_indexes.empty())
-	{
-		FbxGeometryElementNormal* normals_ptr = mesh_ptr->CreateElementNormal();
-		normals_ptr->SetMappingMode(FbxGeometryElementNormal::eByPolygonVertex);
-		normals_ptr->SetReferenceMode(FbxGeometryElement::eIndexToDirect);
-		auto& direct_array = normals_ptr->GetDirectArray();
-		std::for_each(m_normals.begin(), m_normals.end(),
-			[&direct_array](const Geometry::Vector3& elem)
-			{
-				direct_array.Add(FbxVector4(elem.x, elem.y, elem.z));
-			});
-
-		auto& index_array = normals_ptr->GetIndexArray();
-		std::for_each(normal_indexes.begin(), normal_indexes.end(),
-			[&index_array](const uint32_t& idx) { index_array.Add(idx); });
-	}
-
-	// add tangents
-	if (!tangents_idxs.empty())
-	{
-		FbxGeometryElementTangent* normals_ptr = mesh_ptr->CreateElementTangent();
-		normals_ptr->SetMappingMode(FbxGeometryElement::eByPolygonVertex);
-		normals_ptr->SetReferenceMode(FbxGeometryElement::eIndexToDirect);
-		auto& direct_array = normals_ptr->GetDirectArray();
-		std::for_each(m_lighting_normals.begin(), m_lighting_normals.end(),
-			[&direct_array](const Geometry::Vector4& elem)
-			{
-				direct_array.Add(FbxVector4(elem.x, elem.y, elem.z));
-			});
-
-		auto& index_array = normals_ptr->GetIndexArray();
-		std::for_each(tangents_idxs.begin(), tangents_idxs.end(),
-			[&index_array](const uint32_t& idx) { index_array.Add(idx); });
-	}
-
-	mesh_ptr->BuildMeshEdgeArray();*/
 }
